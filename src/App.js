f101e34fa1b6566ddfd22615e5ab9e953bac78de
4CCELA <luxeurr@gmail.com>
2022-06-13 05:33:09 -0700

    created search engine

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictonary />
+          <Dictionary />
         </main>
-        <footer className="text-center">Coded by Danika Piromgraipakd</footer>
+        <footer className="App-footer">Coded by Danika Piromgraipakd</footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Dictionary() {
-  return "Hello from Dictionary";
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}...`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
